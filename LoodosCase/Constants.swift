31ec238e8c6e1c6713c2e99e9346f2797c530844
Oğuz Demirhan <squiretoss17@gmail.com>
2021-08-21 17:16:17 +0300

    Pods and Helper Constants File added

---
+++ LoodosCase/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  LoodosCase
+//
+//  Created by Oguz Demırhan on 21.08.2021.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "ec6eeb36"
+    static let baseURL = "http://www.omdbapi.com/"
+}
---
+++ LoodosCase/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  LoodosCase
+//
+//  Created by Oguz Demırhan on 21.08.2021.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
